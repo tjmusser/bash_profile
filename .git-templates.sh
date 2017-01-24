2847129c9f4a909848e56c4b3c494699cb242a09
TJ Musser <tmusser@gmail.com>
2017-01-24 15:04:33 -0800

    Update .git-templates.sh

--- .git-templates.sh
+++ .git-templates.sh
@@ -4,7 +4,19 @@
 # Set git to globally use templates directory
 git config --global init.templatedir '~/.git-templates'
 
-# Control will enter here if $DIRECTORY doesn't exist.
+# Create template hooks directory if missing
 if [ ! -d ~/.git-templates/hooks ]; then
   mkdir -p ~/.git-templates/hooks
 fi
+
+# Copy all hooks from repo to system template
+./git-templates/hooks/* ~/.git-templates/hooks
+
+# Make hooks executable
+chmod a+x ~/.git-templates/hooks/pre-commit
+chmod a+x ~/.git-templates/hooks/pre-push
+
+# Final step is to run `git init` on any exisiting local repos
+#
+# New repos and cloned repos will have these hooks automatically
+# after installation
