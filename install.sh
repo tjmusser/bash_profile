3d45219d541240e9cc7d1521c16e47c51572f22d
TJ Musser <tmusser@gmail.com>
2017-01-24 15:26:42 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,6 +11,24 @@ cp ./.git-config-setup.sh ~/
 cp ./.git-prompt.sh ~/
 cp ./.git-templates.sh ~/
 
+# Create template hooks directory if missing
+if [ ! -d ~/.git-templates/hooks ]; then
+  mkdir -p ~/.git-templates/hooks
+fi
+
+# Copy all hooks from repo to system template
+cp ./git-templates/hooks/* ~/.git-templates/hooks
+
+# Make hooks executable
+chmod a+x ~/.git-templates/hooks/pre-commit
+chmod a+x ~/.git-templates/hooks/pre-push
+
+# Final step is to run `git init` on any exisiting local repos
+#
+# New repos and cloned repos will have these hooks automatically
+# after installation
+
+# Change to root to load new bash_profile
 cd ~
 
-. ./bash_profile
+. ~/.bash_profile
