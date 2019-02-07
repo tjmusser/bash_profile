9824ea20145a4d0b264674edabb3489a4576a674
tjmusser <tmusser@illumina.com>
2019-02-07 11:06:30 -0800

    Added user prompts and echo statements for feedback on installation

--- .bash_profile
+++ .bash_profile
@@ -1,26 +1,35 @@
 # 
 # Sourcing basic terminal tools for git and mac alises
 
-if [ -f ~/.git-completion.bash ]; then
-  . ~/.git-completion.bash
-fi
-
-if [ -f ~/.git-prompt.sh ]; then
-  . ~/.git-prompt.sh
-fi
+ADDED_TEXT="[ADDED]"
 
 if [ -f ~/.git-config-setup.sh ]; then
   . ~/.git-config-setup.sh
 fi
 
+if [ -f ~/.git-completion.bash ]; then
+  . ~/.git-completion.bash
+  echo "$ADDED_TEXT git-completion.bash"
+fi
+
+if [ -f ~/.git-prompt.sh ]; then
+  . ~/.git-prompt.sh
+  echo "$ADDED_TEXT git-prompt.sh"
+fi
+
 if [ -f ~/.alias.sh ]; then
   . ~/.alias.sh
+  echo "$ADDED_TEXT aliases for terminal commands"
 fi
 
 if [ -f ~/.git-alias.sh ]; then
   sh ~/.git-alias.sh
+  echo "$ADDED_TEXT git aliases"
 fi
 
 if [ -f ~/.bash-settings.sh ]; then
   . ~/.bash-settings.sh
+  echo "$ADDED_TEXT Terminal window styles"
 fi
+
+echo "Done!"
--- .git-config-setup.sh
+++ .git-config-setup.sh
@@ -1,29 +1,56 @@
 #!/bin/bash
 #
 
-# File to be run once to set up some nice standard git global setting
+# Sets up all the standard git config options. Run this file or the `reload_bash` command 
+# to reset to these defaults
 
-# Allow all Git commands to use colored output, if possible
-git config --global --replace-all color.ui auto
+ADDED_TEXT="[ADDED]"
 
-# Tell Git which whitespace problems it should recognize
-git config --global --replace-all core.whitespace trailing-space,space-before-tab
+echo "Global git config settings"
+echo "--------------------------"
+echo -n "Set user.name and user.email for git config? (y/n) "
+read userConfig
+if [[ $userConfig =~ ^[Yy]$ ]]
+then
+  # Ask for and set git username
+  read -p 'git username: ' gitUsername
+  git config --global user.name "$gitUsername"
 
-# Tell git diff to use mnemonic prefixes (index, work tree, commit, object) instead of the standard a and b notation
-git config --global --replace-all diff.mnemonicprefix true
+  # Ask for and set git user email
+  read -p 'git email: ' gitEmail
+  git config --global user.email "$gitEmail"
+  echo "$ADDED_TEXT User info to git config"
+  echo ""
+fi
 
-# When branching off a remote branch, automatically let the local branch track the remote branch
-git config --global --replace-all branch.autosetupmerge true
+echo -n "Set global config settings for git? (y/n) "
+read globalConfig
+if [[ $globalConfig =~ ^[Yy]$ ]]
+then
+  # Allow all Git commands to use colored output, if possible
+  git config --global --replace-all color.ui auto
 
-# When pushing without giving a refspec, push the current branch to its upstream branch. 
-# See the git config man page for more possible options.
-git config --global --replace-all push.default current
+  # Tell Git which whitespace problems it should recognize
+  git config --global --replace-all core.whitespace trailing-space,space-before-tab
 
-# Enable the recording of resolved conflicts, so that identical hunks can be resolved automatically later on.
-git config --global --replace-all rerere.enabled true
+  # Tell git diff to use mnemonic prefixes (index, work tree, commit, object) instead of the standard a and b notation
+  git config --global --replace-all diff.mnemonicprefix true
 
-# Auto update/stage rerere on merge fixes (works on rebase too)
-git config --global --replace-all rerere.autoupdate true
+  # When branching off a remote branch, automatically let the local branch track the remote branch
+  git config --global --replace-all branch.autosetupmerge true
 
-# Set git to globally use templates directory
-git config --global --replace-all init.templatedir '~/.git-templates'
+  # When pushing without giving a refspec, push the current branch to its upstream branch. 
+  # See the git config man page for more possible options.
+  git config --global --replace-all push.default current
+
+  # Enable the recording of resolved conflicts, so that identical hunks can be resolved automatically later on.
+  git config --global --replace-all rerere.enabled true
+
+  # Auto update/stage rerere on merge fixes (works on rebase too)
+  git config --global --replace-all rerere.autoupdate true
+
+  # Set git to globally use templates directory
+  git config --global --replace-all init.templatedir '~/.git-templates'
+
+  echo "$ADDED_TEXT git config global settings"
+fi
