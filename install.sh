5a9076dfb1de413978c273a67f702a976931cf2a
tjmusser <tmusser@illumina.com>
2021-01-22 12:22:16 -0800

    Cleaned up pre-commit and install messages some

--- install.sh
+++ install.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 #
-echo "Starting install of bash settings and git config"
+LINES="------------------------------------------------------------------------------"
+
+# Begin install
+echo "Starting install of global bash settings and global git config"
 echo ""
+
+if test -f ~/.bash_profile; then
+  echo $LINES
+  echo "WARNING: This will overwrite the exisiting bash_profile file you might have created."
+  echo $LINES
+  echo ""
+  read -p "Would you like to proceed? (y/n)" shouldWeProceed
+  echo $shouldWeProceed | grep ^[Yy]$
+  if [ $? -eq 0 ]
+  then
+    exit 0; #THE USER WANTS TO CONTINUE
+  else
+    exit 1; # THE USER DONT WANT TO CONTINUE SO ROLLBACK
+  fi
+fi
+
 sleep 1
 # Copy files
 cp .alias.sh ~/
@@ -11,10 +30,10 @@ cp .git-alias.sh ~/
 cp .git-completion.bash ~/
 cp .git-config-setup.sh ~/
 cp .git-prompt.sh ~/
-echo "[COPIED] settings files to root directory"
+echo "[COPIED] git settings and bash files to user directory"
 
 # Copy all hooks from repo to system template
-echo -n "Install git hook templates? (y/n) "
+echo -n "Install templates for git hooks? (y/n) "
 read installHooks
 echo ""
 if [[ $installHooks =~ ^[Yy]$ ]]
@@ -34,17 +53,17 @@ then
   echo "[ADDED] git hook templates"
   echo ""
   # Final step is to run `git init` on any exisiting local repos
-  echo "------------------------------------------------------------------------------"
+  echo $LINES
   echo "NOTE: To use the installed git hooks on an existing local repo, run 'git init'"
   echo "New and cloned repos will automatically have these hooks"
-  echo "------------------------------------------------------------------------------"
+  echo $LINES
   echo ""
   # New repos and cloned repos will have these hooks automatically
   # after installation
   sleep 2
 fi
 
-if [ -f ~/.git-config-setup.sh ]; then
+if test -f ~/.git-config-setup.sh; then
   . ~/.git-config-setup.sh
 fi
 
@@ -56,4 +75,5 @@ echo "$ADDED_TEXT git-prompt.sh"
 echo "$ADDED_TEXT aliases for terminal commands"
 echo "$ADDED_TEXT git aliases"
 echo "$ADDED_TEXT Terminal window styles"
-echo "Done!"
+echo "[LOADED] bash file to current terminal."
+echo "Ready to use!"
